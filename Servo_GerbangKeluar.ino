e082e2c291ba36c7c927b220d2e75393d4838a09
David Wahyu <22112331+davidwah@users.noreply.github.com>
2018-12-13 10:52:35 +0700

    Update Servo_GerbangKeluar.ino

--- Servo_GerbangKeluar.ino
+++ Servo_GerbangKeluar.ino
@@ -99,3 +99,107 @@ void loop() {
   }
   client.loop();
 }
+
+
+
+
+///////////// PERBAIKAN PALANG ////////////
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Servo.h>
+
+
+
+const char* ssid = "POSKO KETAHANAN INTERNET NKRI";                   //SSID Wifi
+const char* password = "KOPIIRENG";       //Password Wifi
+const char* mqtt_server = "dwp-project.xyz";  // Alamat Broker
+
+
+int servo_pin = D0 ;
+int pos;
+Servo myservo;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(100);
+  // koneksi pada jaringan WIFI
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("Command from MQTT broker is : [");
+  Serial.print(topic);
+  Serial.print(" Rotation angle is:");
+  Serial.print("   ");
+  int p = (char)payload[0] - '0';
+  //  if(p==0)
+  //  {
+  //    myservo.attach(D0);
+  //    myservo.write(0);
+  //    Serial.println(" Servo Close! ");
+  //  }
+  if (p == 1)
+  {
+    myservo.attach(D0);
+    myservo.write(90);
+    Serial.println(" Servo Open! ");
+  }
+    for (pos = 90; pos >= 0; pos -= 90) {
+      myservo.write(pos);
+      delay(5000);
+  }
+  Serial.println();
+}
+//end callback
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+
+    // Create Name ClientId
+    String clientId = "Gerbang Parkir Masuk-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      client.subscribe("parkir/palangMasuk"); //Topik MQTT yang di Subscribe
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // delay 5 detik
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  myservo.attach(D0);  // pin output
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
